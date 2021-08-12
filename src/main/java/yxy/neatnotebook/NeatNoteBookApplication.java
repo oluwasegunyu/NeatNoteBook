e4c375b853e705e8a317bbb3983920ba60db6591
yxy <charlay_yu@163.com>
2021-08-13 06:57:53 +0800

    init

---
+++ src/main/java/yxy/neatnotebook/NeatNoteBookApplication.java
@@ -0,0 +1,13 @@
+package yxy.neatnotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeatNoteBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeatNoteBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yxy/neatnotebook/NeatNoteBookApplicationTests.java
@@ -0,0 +1,13 @@
+package yxy.neatnotebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeatNoteBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

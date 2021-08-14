fdc8fbb6951e3d2d46310251a37b2766a0c2ad89
yxy <charlay_yu@163.com>
2021-08-14 17:15:43 +0800

    hello接口

--- src/main/java/yxy/neatnotebook/config/NeatNoteBookApplication.java
+++ src/main/java/yxy/neatnotebook/config/NeatNoteBookApplication.java
@@ -1,11 +1,13 @@
-package yxy.neatnotebook;
+package yxy.neatnotebook.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("yxy.neatnotebook")
 @SpringBootApplication
 public class NeatNoteBookApplication {
 

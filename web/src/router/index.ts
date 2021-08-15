dd0a1f25cae64405ac37d328c59372fd0c445693
yxy <charlay_yu@163.com>
2021-08-16 07:29:28 +0800

    添加电子书管理页面

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,22 +1,28 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/home.vue'
-import About from '../views/about.vue'
+import home from '../views/home.vue'
+import about from '../views/about.vue'
+import adminEbook from '../views/admin/adminEbook.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: home
   },
   {
     path: '/about',
-    name: 'about',
-    component: About
+    name: 'About',
+    component: about
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
-  }
+  },
+  {
+    path: '/admin/ebook',
+    name: 'adminEbook',
+    component: adminEbook
+  },
 ]
 
 const router = createRouter({

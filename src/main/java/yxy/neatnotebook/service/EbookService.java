ce24d4001d2b98858b004bb6e5f41416dd15d319
yxy <charlay_yu@163.com>
2021-08-17 06:38:29 +0800

    后端分页PageInfo，PageHelper

--- src/main/java/yxy/neatnotebook/service/EbookService.java
+++ src/main/java/yxy/neatnotebook/service/EbookService.java
@@ -1,6 +1,9 @@
 package yxy.neatnotebook.service;
 
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -16,6 +19,9 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
@@ -27,6 +33,10 @@ public class EbookService {
         }
         PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数: {}", pageInfo.getTotal());
+        LOG.info("总页数: {}", pageInfo.getPages());
 
         List<EbookResp> respList = new ArrayList<>();
         for(Ebook ebook: ebookList){

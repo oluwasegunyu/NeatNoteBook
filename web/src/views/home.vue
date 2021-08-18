13099a6c00ff6ee32550653513e03861a07bbde7
yxy <charlay_yu@163.com>
2021-08-18 15:36:40 +0800

    点击首页二级分类显示电子书

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -1,48 +1,45 @@
 <template>
   <a-layout>
     <a-layout-sider width="200" style="background: #fff">
-    <a-menu
+      <a-menu
         mode="inline"
         :style="{ height: '100%', borderRight: 0 }"
         @click="handleClick"
-    >
-      <a-menu-item key="welcome">
-        <router-link :to="'/'">
+      >
+        <a-menu-item key="welcome">
           <MailOutlined />
           <span>欢迎</span>
-        </router-link>
-      </a-menu-item>
-      <a-sub-menu v-for="item in level1" :key="item.id">
-        <template v-slot:title>
-              <span>
-                <user-outlined />
-                {{item.name}}
-              </span>
-        </template>
-        <a-menu-item v-for="child in item.children" :key="child.id">
-          <MailOutlined />
-          <span>{{child.name}}</span>
         </a-menu-item>
-      </a-sub-menu>
-    </a-menu>
-  </a-layout-sider>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined />{{item.name}}</span>
+          </template>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined /><span>{{child.name}}</span>
+          </a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
     <a-layout-content
-      :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
   >
-      <a-list item-layout="vertical" size="large" :grid="{gutter: 20, column: 3}" :data-source="ebooks">
+      <div class="welcome" v-show="isShowWelcome">
+        <h1>欢迎使用甲蛙知识库</h1>
+      </div>
+      <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
         <template #renderItem="{ item }">
-          <a-list-item key="item.title">
+          <a-list-item key="item.name">
             <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component v-bind:is="type" style="margin-right: 8px" />
-            {{ text }}
-          </span>
+              <span v-for="{ type, text } in actions" :key="type">
+                <component v-bind:is="type" style="margin-right: 8px" />
+                {{ text }}
+              </span>
             </template>
             <a-list-item-meta :description="item.description">
               <template #title>
                 <a :href="item.href">{{ item.name }}</a>
               </template>
-              <template #avatar><a-avatar :src="item.cover" /></template>
+              <template #avatar><a-avatar :src="item.cover"/></template>
             </a-list-item-meta>
           </a-list-item>
         </template>
@@ -52,27 +49,30 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, ref} from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
-import { Tool } from "../../util/tool";
 import { message } from 'ant-design-vue';
+import {Tool} from "../../util/tool";
 
-const listData: any = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
+// const listData: any = [];
+// for (let i = 0; i < 23; i++) {
+//   listData.push({
+//     href: 'https://www.antdv.com/',
+//     title: `ant design vue part ${i}`,
+//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+//     description:
+//         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+//     content:
+//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//   });
+// }
+
 export default defineComponent({
   name: 'Home',
-  setup(){
+  setup() {
     const ebooks = ref();
+    // const ebooks1 = reactive({books: []});
+
     const level1 =  ref();
     let categorys: any;
     /**
@@ -94,40 +94,60 @@ export default defineComponent({
       });
     };
 
-    const handleClick = () => {
-      console.log("menu click")
+    const isShowWelcome = ref(true);
+    let categoryId2 = 0;
+
+    const handleQueryEbook = () => {
+      axios.get("/ebook/list", {
+        params: {
+          page: 1,
+          size: 1000,
+          categoryId2: categoryId2
+        }
+      }).then((response) => {
+        const data = response.data;
+        ebooks.value = data.content.list;
+        // ebooks1.books = data.content;
+      });
+    };
+
+    const handleClick = (value: any) => {
+      // console.log("menu click", value)
+      if (value.key === 'welcome') {
+        isShowWelcome.value = true;
+      } else {
+        categoryId2 = value.key;
+        isShowWelcome.value = false;
+        handleQueryEbook();
+      }
+      // isShowWelcome.value = value.key === 'welcome';
     };
 
     onMounted(() => {
       handleQueryCategory();
-      axios.get("/ebook/list").then((response) => {
-        const data = response.data;
-        ebooks.value = data.content;
-        //ebooks1.books = data.content;
-      });
+      // handleQueryEbook();
     });
-
-    const pagination = {
-      onChange: (page: number) => {
-        console.log(page);
-      },
-      pageSize: 3,
-    };
-
-    const actions: Record<string, string>[] = [
-      { type: 'StarOutlined', text: '156' },
-      { type: 'LikeOutlined', text: '156' },
-      { type: 'MessageOutlined', text: '2' },
-    ];
 
     return {
       ebooks,
-      //ebooks2: toRef(ebooks1, "books"),
-      //listData,
-      pagination,
-      actions,
+      // ebooks2: toRef(ebooks1, "books"),
+      // listData,
+      pagination: {
+        onChange: (page: any) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions: [
+        { type: 'StarOutlined', text: '156' },
+        { type: 'LikeOutlined', text: '156' },
+        { type: 'MessageOutlined', text: '2' },
+      ],
+
       handleClick,
       level1,
+
+      isShowWelcome
     }
   }
 });
